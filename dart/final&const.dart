b431781ff844509dcac29c4071ff110584ccd26f
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-03-03 23:28:30 +0530

    commit it

---
+++ dart/final&const.dart
@@ -0,0 +1,14 @@
+void main(){
+  final String country="India";
+  const double pi=3.14;
+  final time=DateTime.now();
+  final students=DateTime.now().hour;
+  // const time = DateTime.now(); ❌ error
+  //"Each section will always have 60 students."
+  const maxStudents = 60;
+  print(maxStudents);
+  print(country);
+  print(pi);
+  print(time);
+  print(students);
+}
---
+++ dart/late&default.dart
@@ -0,0 +1,8 @@
+void main(){
+  late String name;
+  name="Yashsvi";
+  print(name);
+  //If you declare a nullable variable without
+  int? n;
+  print(n);
+}
